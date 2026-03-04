1f867bbd6c731f39c9b6981fe472605d6dfc8a62
l1mus <user01@DESKTOP-G0IBLL4.localdomain>
2026-03-04 11:17:28 +0700

    refactor: refactor the code writing in the calculation function

--- my-proyek/newFolder/process.js
+++ my-proyek/newFolder/process.js
@@ -1,10 +1,4 @@
 const dataString = ["Aku", "adalah", "Kunci"];
 const dataNumber = [4, 2, 5, 7, 1, 7, 4, 9];
 
-const calculateData = (nums) => {
-  const result = 0;
-  for (let i of nums) {
-    result += i;
-  }
-  return result;
-};
+const calculateData = (nums) => nums.reduce((a, b) => a + b, 0);
