9b30471dbe31ee1721571d2f12f0658c8fee307b
l1mus <user01@DESKTOP-G0IBLL4.localdomain>
2026-03-04 11:20:58 +0700

    feat: new feature, add logic transformString function

--- my-proyek/newFolder/process.js
+++ my-proyek/newFolder/process.js
@@ -2,3 +2,11 @@ const dataString = ["Aku", "adalah", "Kunci"];
 const dataNumber = [4, 2, 5, 7, 1, 7, 4, 9];
 
 const calculateData = (nums) => nums.reduce((a, b) => a + b, 0);
+
+const trasnforString = (strings) => {
+  const result = [];
+  for (let i of strings) {
+    result.push(i.toUpperCase());
+  }
+  return result;
+};
