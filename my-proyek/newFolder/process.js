2cee3ea80e072223e7e47aa9e6a48a741e4acaed
l1mus <user01@DESKTOP-G0IBLL4.localdomain>
2026-03-04 11:22:46 +0700

    refactor: refactor the code writing in the transforString  function

--- my-proyek/newFolder/process.js
+++ my-proyek/newFolder/process.js
@@ -3,10 +3,4 @@ const dataNumber = [4, 2, 5, 7, 1, 7, 4, 9];
 
 const calculateData = (nums) => nums.reduce((a, b) => a + b, 0);
 
-const trasnforString = (strings) => {
-  const result = [];
-  for (let i of strings) {
-    result.push(i.toUpperCase());
-  }
-  return result;
-};
+const trasnforString = (strings) => strings.map((value) => value.toUpperCase());
